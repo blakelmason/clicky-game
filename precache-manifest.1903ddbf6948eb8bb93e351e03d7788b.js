74d80152ab351fce6870865e7cd2209571723e25
Blake Mason <blakelmason@gmail.com>
2018-11-14 12:23:38 -0800

    Updates

---
+++ precache-manifest.1903ddbf6948eb8bb93e351e03d7788b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a27f196562640cab82f46711f5ed0c5f",
+    "url": "/clicky-game/static/media/2.a27f1965.jpg"
+  },
+  {
+    "revision": "d23d1dc612196799df22",
+    "url": "/clicky-game/static/css/main.8c920ec5.chunk.css"
+  },
+  {
+    "revision": "281b72a3994778a839dd",
+    "url": "/clicky-game/static/js/1.281b72a3.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "438b12a5e5a29bc0ab3f3e3c7380a72a",
+    "url": "/clicky-game/static/media/background.438b12a5.png"
+  },
+  {
+    "revision": "cdc81329fbbd644cc6c54d39c0fa7e56",
+    "url": "/clicky-game/static/media/1.cdc81329.jpg"
+  },
+  {
+    "revision": "be4333155f7d061189434c3cd6cda98b",
+    "url": "/clicky-game/static/media/10.be433315.jpg"
+  },
+  {
+    "revision": "7a2deef85855755d73b5c56d2c4a11cd",
+    "url": "/clicky-game/static/media/11.7a2deef8.jpg"
+  },
+  {
+    "revision": "a2fadebe9c9cae44c9d62127b0851073",
+    "url": "/clicky-game/static/media/12.a2fadebe.jpg"
+  },
+  {
+    "revision": "d23d1dc612196799df22",
+    "url": "/clicky-game/static/js/main.d23d1dc6.chunk.js"
+  },
+  {
+    "revision": "914731c5fa75c18d8a02d77dc8bceed6",
+    "url": "/clicky-game/static/media/3.914731c5.jpg"
+  },
+  {
+    "revision": "245fb01f13a0ab2332b221db2c2c6661",
+    "url": "/clicky-game/static/media/4.245fb01f.jpg"
+  },
+  {
+    "revision": "e7a9b085c37f665e073b36cad29e7fa4",
+    "url": "/clicky-game/static/media/5.e7a9b085.jpg"
+  },
+  {
+    "revision": "356457f8a5fa42ea308fc21ef0f00b7b",
+    "url": "/clicky-game/static/media/6.356457f8.jpg"
+  },
+  {
+    "revision": "d3c429a1cbdafc1d22d38d2b5018b706",
+    "url": "/clicky-game/static/media/7.d3c429a1.jpg"
+  },
+  {
+    "revision": "a473a89f1612d804b1ee1d26882164cd",
+    "url": "/clicky-game/static/media/8.a473a89f.jpg"
+  },
+  {
+    "revision": "4333af8fa223aca7be3ae6ecbd6e4a86",
+    "url": "/clicky-game/static/media/9.4333af8f.jpg"
+  },
+  {
+    "revision": "7965819e8fbdfff652276528ad456a2d",
+    "url": "/clicky-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-clicky-game/precache-manifest.4a537c3aaf838516d9feb6c0512f74b0.js"
+  "/clicky-game/precache-manifest.1903ddbf6948eb8bb93e351e03d7788b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-clicky-game/index.html", {
+workbox.routing.registerNavigationRoute("/clicky-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
